1662338f5b6887d848774d3ee9d4604fac452523
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2023-02-02 09:44:13 +0100

    dichiarato componente stand alone

---
+++ src/components/custom-template/custom-template.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-template',
+  templateUrl: './custom-template.component.html',
+  standalone: true,
+  styleUrls: ['./custom-template.component.css']
+})
+export class CustomTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { CustomTemplateComponent } from './components/custom-template/custom-template.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CustomTemplateComponent],
   template: `
     <h1>Hello from {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
       Learn more about Angular 
     </a>
+    
+    <app-custom-template></app-custom-template>
   `,
 })
 export class App {
